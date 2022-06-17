102fc5995a08664599a9408d6998eb7f273f4f83
Jasef21 <jasef.huachambe@gmail.com>
2022-06-17 01:11:28 -0500

    se agrego container a detail

--- time_to_travel_app/lib/Pages/detail_page.dart
+++ time_to_travel_app/lib/Pages/detail_page.dart
@@ -82,143 +82,152 @@ class _DetailPageState extends State<DetailPage> {
                     ],
                   ),
                 ),
-                Positioned(
-                  top: 330,
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                      left: 20,
-                      right: 20,
-                      top: 30,
-                    ),
-                    width: MediaQuery.of(context).size.width,
-                    height: double.maxFinite,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30)),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Expanded(
-                                child: AutoSizeText(
-                              detail.places.name,
-                              style: const TextStyle(fontSize: 30),
-                              maxLines: 2,
-                            )),
-                            AppLargeText(
-                              text: "\$" + detail.places.price.toString(),
-                              color: AppColors.secundaryColor,
-                              size: 30,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Row(
-                          children: [
-                            Icon(Icons.location_on,
-                                color: AppColors.secundaryColor, size: 25),
-                            const SizedBox(height: 5),
-                            AppText(
-                              text: detail.places.location,
-                              color: AppColors.textColor1,
-                              size: 20,
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        Row(
-                          children: [
-                            Wrap(
-                              children: List.generate(5, (index) {
-                                return Icon(
-                                  Icons.star_rate_rounded,
-                                  color: index < detail.places.stars
-                                      ? AppColors.starColor
-                                      : AppColors.textColor2,
-                                  size: 30,
-                                );
-                              }),
-                            ),
-                            const SizedBox(width: 5),
-                            AppText(
-                              text:
-                                  "(" + detail.places.stars.toString() + ".0)",
-                              color: AppColors.textColor2,
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 25),
-                        //CAMBIAR POR CARRUSEL DE IMAGENES SACAR DE APP_PELICULAS
-                        AppLargeText(
-                          text: "Te gustó la experiencia?",
-                          color: Colors.black.withOpacity(0.8),
-                          size: 20,
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        AppText(
-                          text: "Recomienda este atractivo a los demás",
-                          color: AppColors.mainTextColor,
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Wrap(
-                          children: List.generate(
-                            5,
-                            (index) {
-                              return InkWell(
-                                onTap: () {
-                                  setState(
-                                    () {
-                                      selectedIndex = index;
-                                      voto = selectedIndex + 1;
-                                      print("Nivel recomendacion: $voto");
-                                    },
-                                  );
-                                },
-                                child: Container(
-                                  margin: const EdgeInsets.only(right: 12),
-                                  child: AppButtons(
-                                    size: 50,
-                                    color: selectedIndex == index
-                                        ? Colors.white
-                                        : Colors.black,
-                                    backgroundColor: selectedIndex == index
-                                        ? Colors.black
-                                        : AppColors.secundarybuttonColor,
-                                    borderColor: selectedIndex == index
-                                        ? Colors.black
-                                        : AppColors.secundarybuttonColor,
-                                    text: (index + 1).toString(),
-                                  ),
-                                ),
-                              );
-                            },
+                Container(
+                  margin:const EdgeInsets.only(top: 330),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30)),
+                  ),
+                  child: Positioned(
+                    top: 330,
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        top: 30,
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      height: double.maxFinite,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(60),
+                            topRight: Radius.circular(60)),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Expanded(
+                                  child: AutoSizeText(
+                                detail.places.name,
+                                style: const TextStyle(fontSize: 30),
+                                maxLines: 2,
+                              )),
+                              AppLargeText(
+                                text: "\$" + detail.places.price.toString(),
+                                color: AppColors.secundaryColor,
+                                size: 30,
+                              ),
+                            ],
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        AppLargeText(
-                          text: "Descripción",
-                          color: Colors.black.withOpacity(0.8),
-                          size: 30,
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        AppText(
-                          text: detail.places.description,
-                          color: AppColors.mainTextColor,
-                        ),
-                      ],
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Icon(Icons.location_on,
+                                  color: AppColors.secundaryColor, size: 25),
+                              const SizedBox(height: 5),
+                              AppText(
+                                text: detail.places.location,
+                                color: AppColors.textColor1,
+                                size: 20,
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Wrap(
+                                children: List.generate(5, (index) {
+                                  return Icon(
+                                    Icons.star_rate_rounded,
+                                    color: index < detail.places.stars
+                                        ? AppColors.starColor
+                                        : AppColors.textColor2,
+                                    size: 30,
+                                  );
+                                }),
+                              ),
+                              const SizedBox(width: 5),
+                              AppText(
+                                text:
+                                    "(" + detail.places.stars.toString() + ".0)",
+                                color: AppColors.textColor2,
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 25),
+                          //CAMBIAR POR CARRUSEL DE IMAGENES SACAR DE APP_PELICULAS
+                          AppLargeText(
+                            text: "Te gustó la experiencia?",
+                            color: Colors.black.withOpacity(0.8),
+                            size: 20,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          AppText(
+                            text: "Recomienda este atractivo a los demás",
+                            color: AppColors.mainTextColor,
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Wrap(
+                            children: List.generate(
+                              5,
+                              (index) {
+                                return InkWell(
+                                  onTap: () {
+                                    setState(
+                                      () {
+                                        selectedIndex = index;
+                                        voto = selectedIndex + 1;
+                                        print("Nivel recomendacion: $voto");
+                                      },
+                                    );
+                                  },
+                                  child: Container(
+                                    margin: const EdgeInsets.only(right: 12),
+                                    child: AppButtons(
+                                      size: 50,
+                                      color: selectedIndex == index
+                                          ? Colors.white
+                                          : Colors.black,
+                                      backgroundColor: selectedIndex == index
+                                          ? Colors.black
+                                          : AppColors.secundarybuttonColor,
+                                      borderColor: selectedIndex == index
+                                          ? Colors.black
+                                          : AppColors.secundarybuttonColor,
+                                      text: (index + 1).toString(),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          AppLargeText(
+                            text: "Descripción",
+                            color: Colors.black.withOpacity(0.8),
+                            size: 30,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          AppText(
+                            text: detail.places.description,
+                            color: AppColors.mainTextColor,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
